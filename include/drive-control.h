cfe00e82e10209d5e56662f5ca35b0fb445cf345
Brandon Sutherland <brandonsutherland2@gmail.com>
2021-10-13 12:55:12 -0600

    Additional motor-control work

--- include/drive-control.h
+++ include/drive-control.h
@@ -4,6 +4,10 @@
 #include "motor-control.h"
 
 
+// Hardware defined-values
+const double _DriveControl__wheel_diameter = 8.0;  //cm
+const int _DriveControl__wheel_distance = 22.5;  //cm
+
 
 
 
--- include/motor-control.h
+++ include/motor-control.h
@@ -1,7 +1,9 @@
 #ifndef MOTOR_CONTROL_H_
 #define MOTOR_CONTROL_H_
 
-
+// Hardware-defined values
+const double _MotorControl__left_motor_stepsize = 0.5;
+const double _MotorControl__right_motor_stepsize = 0.5;
 
 
 
