046d0b1db6d5b8e7180d1ed2d0e529461648fe4c
Brandon Sutherland <brandonsutherland2@gmail.com>
2021-10-14 17:32:05 -0600

    Added code for demo 2

--- src/main.c
+++ src/main.c
@@ -12,10 +12,18 @@ int main(int argc, char** argv) {
     ANSB = 0;
 
     StepperMotor__init();
-    StepperMotor__rotate(2, 1, 0.25, 0.5);
-    StepperMotor__rotate(1, 0, 0.5, 1);
+    StepperMotor__rotate(2, 1, 1, 2);
+    StepperMotor__rotate(1, 0, 1, 2);
     StepperMotor__completeRotations();
-    StepperMotor__rotate(2, 1, 0.5, 0);
+    
+    StepperMotor__rotate(2, 1, 0.5, 1.33);
+    StepperMotor__completeRotations();
+    
+    StepperMotor__rotate(2, 1, 1, 2);
+    StepperMotor__rotate(1, 0, 1, 2);
+    StepperMotor__completeRotations();
+    
+    
     
     while (1);
     return 0;
