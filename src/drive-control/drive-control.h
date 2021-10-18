a2ba7737b09fe99b7cd638b8f518c1aa2a120f22
akwelker <54192150+akwelker@users.noreply.github.com>
2021-10-18 13:55:22 -0600

    Began work on the drive control library

--- src/drive-control/drive-control.h
+++ src/drive-control/drive-control.h
@@ -1,13 +1,50 @@
+/* Mechawreckers    MEEN 330   Oct 2021
+
+drive-control.h -- A library that contains high level commands to control the robot, such as 
+"go forward" or "rotate". methods will include commands for rates and distances 
+*/
+
+
 #ifndef DRIVE_CONTROL_H_
 #define DRIVE_CONTROL_H_
 
-#include "stepper-motor/stepper-motor.h"
+#include "../stepper-motor/stepper-motor.h"
 
 // Hardware defined-values
 #define _DriveControl__wheel_diameter = 8.0;  //cm
 #define _DriveControl__wheel_distance = 22.5;  //cm
 
 
+// ---- Driving Methods ----
+void driveForward(float rate);
+
+void driveBackwards(float rate);
+
+void driveForward(float rate, float distance);
+
+void driveBackwards(float rate, float distance);
+
+// ---- Rotating Methods ---- 
+
+void rotateCW(float rate);
+
+void rotateCCW(float rate);
+
+void rotateCW(float rate, float degrees);
+
+void rotateCCW(float rate, float degrees);
+
+
+
+// ---- Turning methods ----
+
+void turnRight(float rate);
+
+void turnLeft(float rate);
+
+void turnRight(float rate, float degrees, float radius);
+
+void turnLeft(float rate, float degrees, float radius);
 
 
 #endif
