62f930dfb32e1154cb1f126f338571ba846c38cf
akwelker <54192150+akwelker@users.noreply.github.com>
2021-10-27 22:03:09 -0600

    initial hardware testing

--- src/main.c
+++ src/main.c
@@ -6,21 +6,40 @@ main.c -- the code that the robot executes
 
 #include <xc.h>
 
-#include "drive-control/drive-control.h"
+#include "analog-read/analog-read.h"
 
 
-#pragma config FNOSC = FRC  // Update value in global-params if changed
 
-int main(int argc, char **argv) {
+
+#pragma config FNOSC = FRCDIV  // Update value in global-params if changed
+
+int main(int argc, char **argv) 
+{
+    _RCDIV = 0b101;
     // Sets all pin registers to 0
     TRISA = 0;
     TRISB = 0;
     ANSA = 0;
     ANSB = 0;
+    
+    analog_init();
+  
+    setup_analog(0);
+    setup_analog(1);
+    setup_analog(2);
+    setup_analog(3);
+    
 
-
-    turnRight(2 * 3.1415 / 15.0, 15.0);
-
-    while (1);
+    while (1)
+    {
+        if(read_analog_pin(5) > 4095/2)
+        {
+            _LATA2 = 1;
+        }
+        else
+        {
+            _LATA2 = 1;
+        }
+    }
     return 0;
 }
