6818e22807bdaa0909d512ecc07a81fc330879cd
akwelker <54192150+akwelker@users.noreply.github.com>
2021-11-06 12:58:48 -0600

    made state machine for LED seeking again

--- src/main.c
+++ src/main.c
@@ -11,6 +11,11 @@ main.c -- the code that the robot executes
 #pragma config OSCIOFNC = OFF // enable pin 8,10
 #pragma config SOSCSRC = DIG // enable pin 9
 
+#define THRESHOLD 500
+
+
+enum STATE {SEEKING,  FOUND}; // state in our state machine
+
 
 void main_init();
 
@@ -19,8 +24,47 @@ int main(int argc, char **argv)
     main_init();
 
     DriveControl__init();
-    DriveControl__rotateCW_dist(90, 180);
-    while (DriveControl__checkDriveStatus());
+    
+    
+    analog_init();
+    setup_analog(4);
+    
+    int current_ir_val = 0;
+    int prev_ir_val = 0;
+    
+    // begin state machine
+    
+    int state = FOUND;
+    
+    while(1)
+    {
+        // get all our info
+        prev_ir_val = current_ir_val;
+        
+        int ave = 0;
+        for(int i  = 0; i < 1000; i++)
+        {
+            ave += read_analog(4);
+        }
+        
+        current_ir_val = ave/1000; // current val is an average of 1000 readings
+        
+        // choose current state
+        if(state == SEEKING)
+        {
+            if(current_ir_val >= THRESHOLD && current_ir_val <= prev_ir_val) // if we're above our limit and decreasing
+            {
+                DriveControl__stopMovement();
+            }
+        }
+        else if(state == FOUND)
+        {
+            if(current_ir_val < THRESHOLD) // if we lose track of the LED, then transition
+            {
+                DriveControl__rotateCW(2*3.1415/8);
+            }
+        }
+    }
 
     return 0;
 }
