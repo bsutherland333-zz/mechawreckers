2e38490450bb6e44858bd87ea00eb675882fde00
akwelker <54192150+akwelker@users.noreply.github.com>
2021-11-09 14:02:20 -0700

    Made pseudocode (+ real code for transitions) for state machine that should follow a line to the goal

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@ main.c -- the code that the robot executes
 */
 
 #include <xc.h>
+#include "global-params.h"
 #include "analog-read/analog-read.h"
 #include "drive-control/drive-control.h"
 
@@ -14,7 +15,7 @@ main.c -- the code that the robot executes
 #define THRESHOLD 500
 
 
-enum STATE {ON_LINE, RIGHT_OF_LINE, LEFT_OF_LINE, OFF_LINE_IR, OFF_LINE_NO_IR}; // state in our state machine
+enum STATE {ON_LINE, RIGHT_OF_LINE, LEFT_OF_LINE, OFF_LINE_IR, OFF_LINE_NO_IR, ON_CROSS}; // state in our state machine
 
 
 void main_init();
@@ -27,47 +28,138 @@ int main(int argc, char **argv)
     
     
     analog_init();
-    setup_analog(14);
     
-    int current_ir_val = 0;
-    int prev_ir_val = 0;
     
-    // begin state machine
-    
-    int state = FOUND;
+    // begin state machine assuming no ir and off line
+    int state = OFF_LINE_NO_IR;
+    DriveControl__rotateCW(2*PI/15);
     
     while(1)
     {
-        // get all our info
-        prev_ir_val = current_ir_val;
+        // Read in QRD right
+        // Read in QRD left
         
-        double ave = 0;
-        for(int i  = 0; i < 100; i++)
-        {
-            ave += (double)read_analog_pin(8)/100;
-        }
+        // set previous IR value as current value
+        // set current IR value as average of n inputs
         
-        current_ir_val = (int)ave; // current val is an average of 1000 readings
-        
-        // choose current state
-        if(state == SEEKING)
+        // Observe current state
+        if(state == OFF_LINE_NO_IR)
         {
             if(current_ir_val >= THRESHOLD && prev_ir_val >= current_ir_val) // if we're above our limit and decreasing
             {
-                DriveControl__stopMovement();
-                _LATB2 = 1;
-                state = FOUND;
+                DriveControl__stopMovement(); // stop the robot
+                DriveControl__driveForwards(5); // begin to move forwards
+
+                state = OFF_LINE_IR; // change state
             }
         }
-        else if(state == FOUND)
+        else if(state == OFF_LINE_IR)
         {
-            if(current_ir_val < THRESHOLD) // if we lose track of the LED, then transition
+            if(0) // if RIGHT QRD is on
             {
-                DriveControl__rotateCW(2*3.1415/8);
-                _LATB2 = 0;
-                state = SEEKING;
+                DriveControl__stopMovement(); // stop the robot
+                DriveControl__turnRight(5,50); // begin to move forwards
+                
+                state = RIGHT_OF_LINE; // transition of right of line state
+            }
+            else if(0) // if LEFT QRD is on
+            {
+                DriveControl__stopMovement(); // stop the robot
+                DriveControl__turnLeft(5,50); // begin to veer left
+                
+                state = LEFT_OF_LINE; // transition to left of line state 
+            }
+            else if(0) // if Both QRD are on
+            {
+                DriveControl__stopMovement(); // stop the robot
+                DriveControl__driveForwards(5); // begin to move forwards
+                
+                state = ON_LINE; // transition to left of line state 
             }
         }
+        else if(state == ON_LINE)
+        {
+            if(0) // if RIGHT QRD is on
+            {
+                DriveControl__stopMovement(); // stop the robot
+                DriveControl__turnRight(5,50); // begin to veer left
+                
+                state = LEFT_OF_LINE; // transition to left of line state 
+            }
+            else if(0) // if LEFT QRD is on
+            {
+                DriveControl__stopMovement(); // stop the robot
+                DriveControl__turnLeft(5,50); // begin to veer left
+                
+                state = RIGHT_OF_LINE; // transition to left of line state 
+            }
+            else if(0) // if Both QRD are off
+            {
+                DriveControl__stopMovement();
+                DriveControl__rotateCW(2*PI/15); // begin to turn right to find the LED
+                
+                state == OFF_LINE_NO_IR;
+            }
+        }
+        else if(state == RIGHT_OF_LINE)
+        {
+            if(0) // if RIGHT QRD is on
+            {
+                DriveControl__stopMovement(); // stop the robot
+                DriveControl__turnLeft(5,50); // begin to veer left
+                
+                state = LEFT_OF_LINE; // transition to left of line state 
+            }
+            else if(0) // if Both QRD are on
+            {
+                DriveControl__stopMovement(); // stop the robot
+                DriveControl__driveForwards(5); // begin to go forward
+                
+                state = ON_LINE; // transition to left of line state 
+            }
+            else if(0) // if Both QRD are off
+            {
+                DriveControl__stopMovement(); // stop the robot
+                DriveControl__rotateCCW(5); // begin to rotate left
+                
+                state = OFF_LINE_NO_IR; // transition to left of line state 
+            }            
+        }
+        else if(state == LEFT_OF_LINE)
+        {
+             if(0) // if LEFT QRD is on
+            {
+                DriveControl__stopMovement(); // stop the robot
+                DriveControl__turnRight(5,50); // begin to veer right
+                
+                state = RIGHT_OF_LINE; // transition to left of line state 
+            }
+            else if(0) // if Both QRD are on
+            {
+                DriveControl__stopMovement(); // stop the robot
+                DriveControl__driveForwards(5); // begin to go forward
+                
+                state = ON_LINE; // transition to left of line state 
+            }
+            else if(0) // if Both QRD are off
+            {
+                DriveControl__stopMovement(); // stop the robot
+                DriveControl__rotateCCW(5); // begin to rotate left
+                
+                state = OFF_LINE_NO_IR; // transition to left of line state 
+            }             
+        }
+        else if(state == ON_CROSS) // the accept state
+        {
+            DriveControl__stopMovement(); // stop the motors
+            break; // accept the state machine and break the loop
+        }
+        
+    }
+    
+    while(1) // STOP ALL
+    {
+        // do nothing
     }
 
     return 0;
