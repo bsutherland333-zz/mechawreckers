722e46013ea515fb3d71b8e1a6d8460677c8300f
Brandon Sutherland <brandonsutherland2@gmail.com>
2021-11-11 15:18:35 -0700

    Added two additional main functions for separate turret & base development

---
+++ src/dev-main.c
@@ -0,0 +1,33 @@
+/* Mechawreckers    MEEN 330   Oct. 2021 2
+
+dev-main.c -- Main function for developmental purposes
+*/
+
+#include <xc.h>
+#include "drive-control/drive-control.h"
+
+#pragma config FNOSC = FRCDIV  // Update value in global-params if changed
+#pragma config OSCIOFNC = OFF // enable pin 8,10
+#pragma config SOSCSRC = DIG // enable pin 9
+
+
+void main_init();
+
+int main(int argc, char **argv)
+{
+    main_init();
+
+
+
+    return 0;
+}
+
+void main_init()
+{
+    // Sets all pins to be output
+    TRISA = 0;
+    TRISB = 0;
+    // Sets all pins to be digital
+    ANSA = 0;
+    ANSB = 0;
+}
