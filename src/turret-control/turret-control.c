317f3c5629b45a90041b60b864a2f5721d01d17a
Brandon Sutherland <brandonsutherland2@gmail.com>
2021-11-11 18:24:26 -0700

    Initial turret control write

--- src/turret-control/turret-control.c
+++ src/turret-control/turret-control.c
@@ -2,3 +2,68 @@
 
 #include "servo-control/servo-control.h"
 #include "pwm-control/pwm-control.h"
+#include "global-params.h"
+
+#define MOTOR_FREQUENCY 1000.0
+
+void TurretControl__init()
+{
+    TurretControl__setTurretPosition(2);
+    __setTriggerLoad();
+}
+
+void TurretControl__setTurretPosition(int position)
+{
+    switch (position)
+    {
+        case 1:
+            ServoControl__setAngle(1, 0);
+            break;
+        case 2:
+            ServoControl__setAngle(1, 90);
+            break;
+        case 3:
+            ServoControl__setAngle(1, 180);
+            break;
+    }
+}
+
+void TurretControl__setMotorSpeed_black()
+{
+    __setMotorSpeed(1.0);
+}
+
+void TurretControl__setMotorSpeed_white()
+{
+    __setMotorSpeed(1.0);
+}
+
+void TurretControl__disableMotor()
+{
+    __setMotorSpeed(0.0);
+}
+
+void __setMotorSpeed(float motor_speed)
+{
+    float period = CLOCK_HZ / MOTOR_FREQUENCY - 1;
+    float duty_cycle = CLOCK_HZ / MOTOR_FREQUENCY * motor_speed;
+
+    PWMControl__init3(duty_cycle, period);
+}
+
+void TurretControl__activateTrigger()
+{
+    __setTriggerFire();
+    Timer__wait(2.0);
+    __setTriggerLoad();
+}
+
+void __setTriggerFire()
+{
+    ServoControl__setAngle(2, 70);
+}
+
+void __setTriggerLoad()
+{
+    ServoControl__setAngle(2, 125);
+}
--- src/turret-control/turret-control.h
+++ src/turret-control/turret-control.h
@@ -1,6 +1,17 @@
 #ifndef TURRET_CONTROL_H_
 #define TURRET_CONTROL_H_
 
+void TurretControl__init();
 
+void TurretControl__setTurretPosition(int position);
+
+void TurretControl__setMotorSpeed_black();
+void TurretControl__setMotorSpeed_white();
+void TurretControl__disableMotor();
+void __setMotorSpeed(float motor_speed);
+
+void TurretControl__activateTrigger();
+void __setTriggerFire();
+void __setTriggerLoad();
 
 #endif // TURRET_CONTROL_H_
